593424410abfe31a44986219981de77516ac0075
Alyona Pinigina <alenakurakova59@gmail.com>
2023-11-14 16:37:41 +0300

    Модель Post, сайт администрирования

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    class Status(models.TextChoices): 
+        DRAFT = 'DF', 'Daft' # черновик поста
+        PUBLISHED = 'PB', 'Published' # варианты статуса, пары "значение-метка"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250) # короткая метка
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE, # при удалении пользователя удалятся все его посты
+                               related_name='blog_posts') # для обращения к объектам из объекта User, используя обозначение user.blog_posts
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) # дата будет обновляться автоматически
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta: # метаданные модели
+        ordering = ['-publish'] # если в запросе не указан порядок сортировки постов, задается обратный порядок (сначала новые) по полю publish
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self) -> str:
+        return self.title
