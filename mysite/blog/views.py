23cfb64112347f9b7b390bd82228cae952a8fb42
Alena <alenakurakova59@gmail.com>
2023-11-24 14:01:26 +0300

    templates list, detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,10 +5,12 @@ from django.http import Http404
 def post_list(request):
     posts = Post.published.all()
     return render(request, 'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'latest_post': posts.latest('id')})
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
     return render(request, 'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'other_posts': Post.objects.all()[:3]}) # returns the first 3 objects
